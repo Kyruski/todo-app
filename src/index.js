65d2d4cd751bd9dcdbabcf0dfcc5e0be4b925d8a
kyruski <jamescboyett@gmail.com>
2021-04-27 15:56:56 -0700

    Running into issue with webpack loaders

---
+++ src/index.js
@@ -0,0 +1,5 @@
+console.log('Hello World!');
+import * as ReactDOM from "react-dom"
+
+
+ReactDOM.render(<App />, document.getElementById("app"));
--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,39 @@
+// Generated using webpack-cli http://github.com/webpack-cli
 const path = require('path');
-
-const SRC_DIR = path.join(__dirname, 'src');
-const DIST_DIR = path.join(__dirname, 'src');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: `${SRC_DIR}/App.tsx`,
-  output: {
-    filename: 'index.js',
-    path: SRC_DIR,
-  },
-  module: {
-    rules: [
-      {
-        include: SRC_DIR,
-        test: /\.tsx?/,
-        loader: 'ts-loader'
-      }
-    ]
-  },
-  resolve: {
-    extensions: [ '.tsx', '.ts', '.js', '.jsx' ],
-  }
-}
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+        open: true,
+        host: 'localhost',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'index.html',
+        }),
+
+        // Add your plugins here
+        // Learn more obout plugins from https://webpack.js.org/configuration/plugins/
+    ],
+    module: {
+        rules: [
+            {
+                test: /\\.(js|jsx)$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+            },
+            {
+                test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/,
+                type: 'asset',
+            },
+
+            // Add your rules for custom modules here
+            // Learn more about loaders from https://webpack.js.org/loaders/
+        ],
+    },
+};
