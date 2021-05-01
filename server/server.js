aca393f3e5a2f834b83a9cdc7423cc17ec1be0a8
kyruski <jamescboyett@gmail.com>
2021-04-30 20:34:02 -0700

    Switches from Apollo Server to Apollo Server w/ Express

--- server/server.js
+++ server/server.js
@@ -1,11 +1,16 @@
-const { ApolloServer } = require("apollo-server");
+const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
 const { typeDefs } = require('./gql/todo-schema');
 const { resolvers } = require('./gql/resolvers');
 
+const port = 3456;
+
+const app = express();
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
-server.listen()
-  .then(({ url }) => {
-  console.log(`Server ready at ${url}`);
+server.applyMiddleware({app, path: '/graphql'});
+
+app.listen(port, () => {
+  console.log(`Server ready at http://localhost:${port}/ \n GraphQL at: http://localhost:${port}/graphql/`);
 });
