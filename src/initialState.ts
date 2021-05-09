7cd4eb02dd636388046290ce5ff602a4e72abcc7
kyruski <jamescboyett@gmail.com>
2021-05-09 16:49:09 -0700

    Brings merge up to date with origin

--- src/initialState.ts
+++ src/initialState.ts
@@ -1,6 +1,1 @@
-<<<<<<< HEAD
 export const initialState = [{id: 123, title: '', description: '', completed: false}];
-=======
-
-export const initialState = [{id: 123, title: '', description: '', completed: false}];
->>>>>>> ba7051bbc189adb7280d1901d271a2d899b1be17
